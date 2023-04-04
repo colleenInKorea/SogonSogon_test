40070bfc5389f8f62fdab5a7d45988bebfba65a8
colleenInKorea <92035417+colleenInKorea@users.noreply.github.com>
2023-04-04 23:22:45 +0900

    test:일단 한번 해본다. (무중단 배포 )

--- src/main/java/com/sparta/sogonsogon/SogonSgonApplication.java
+++ src/main/java/com/sparta/sogonsogon/SogonSgonApplication.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
@@ -11,8 +12,14 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @OpenAPIDefinition(servers = {@Server(url = "https://sogonsogon.shop", description = "Default Server URL") ,@Server(url = "http://localhost:8080", description = "Local Server URL"),@Server(url= "http://52.79.151.2:8080", description = "http서버 입니다. ")})
 public class SogonSgonApplication {
 
+    public static final String APPLICATION_LOCATIONS = "spring.config.location="
+            + "classpath:application.properties,"
+            + "/app/config/springboot-webservice/real-application.properties";
+
     public static void main(String[] args) {
-        SpringApplication.run(SogonSgonApplication.class, args);
+        new SpringApplicationBuilder(SogonSgonApplication.class)
+                .properties(APPLICATION_LOCATIONS)
+                        .run(args);
     }
 
 }
