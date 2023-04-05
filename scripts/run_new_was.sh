684415308668fd77f71705187c3e5b035a25cf38
colleenInKorea <92035417+colleenInKorea@users.noreply.github.com>
2023-04-06 02:40:31 +0900

    원상 복구

--- scripts/run_new_was.sh
+++ scripts/run_new_was.sh
@@ -1,5 +1,5 @@
 PROJECT_ROOT="/home/ubuntu/app"
-JAR_FILE="$PROJECT_ROOT/spring-webapp.jar"
+JAR_FILE="$PROJECT_ROOT/SogonSogon-0.0.1-SNAPSHOT.jar"
 
 APP_LOG="$PROJECT_ROOT/application.log"
 ERROR_LOG="$PROJECT_ROOT/error.log"
