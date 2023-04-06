685fb98c4948bdabded3909eca462461655b912b
colleenInKorea <92035417+colleenInKorea@users.noreply.github.com>
2023-04-07 03:17:12 +0900

    무중단배포 + https 실험 :Test

--- src/main/java/com/sparta/sogonsogon/web/controller/WebRestController.java
+++ src/main/java/com/sparta/sogonsogon/web/controller/WebRestController.java
@@ -1,10 +1,12 @@
 package com.sparta.sogonsogon.web.controller;
 
 import com.sparta.sogonsogon.audioclip.service.AudioClipService;
+import com.sparta.sogonsogon.dto.StatusResponseDto;
 import com.sparta.sogonsogon.member.dto.MemberOneResponseDto;
 import com.sparta.sogonsogon.member.service.MemberService;
 import lombok.AllArgsConstructor;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +20,7 @@ public class WebRestController {
     private final MemberService memberService;
 
     @GetMapping("/profile")
-    public List<MemberOneResponseDto> getProfile(){
-        return memberService.getAll();
+    public StatusResponseDto<List<MemberOneResponseDto>> getProfile(){
+        return StatusResponseDto.success(HttpStatus.OK, memberService.getAll());
     }
 }
